6a16b0a6196a35ca0d9331127a99791c5c69a0a4
Vitamin <86688525+VitaminA-plays@users.noreply.github.com>
2022-09-15 18:37:27 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string[] readings = System.IO.File.ReadAllLines(@"C:\Users\Axion\source\repos\adventofcode-day1\adventofcode-day1\measurements.txt");
+
+int totalCount = readings.Count();
+int[] measurements = new int [totalCount];
+
+for (int line = 0; line < totalCount; line++)
+{
+    measurements[line] = int.Parse(readings[line]); 
+}
+
+int counter = 0;
+int last = measurements[0];
+
+foreach (int measurement in measurements)
+{
+    if (measurement > last)
+    {
+        counter++;
+    }
+    last = measurement;
+}
+
+Console.WriteLine(counter.ToString());
